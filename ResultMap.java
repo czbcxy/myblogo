768063522cf3d3ac343aecf21466564d9ed78e6a
chengzhengzheng <chengzhengzheng@mart>
2018-12-24 23:41:28 +0800

    上传文件

---
+++ ResultMap.java
@@ -0,0 +1,12 @@
+package me.zbl.fullstack.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ResultMap {
+    private static String success;
+    private static String message;
+    private static String url;
+}
