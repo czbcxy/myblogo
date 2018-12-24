30e29ca1bc1b2a8e8c9cd8b839626c8deaa1d383
chengzhengzheng <chengzhengzheng@mart>
2018-12-24 23:45:58 +0800

    更新文件 FullstackApplication.java

--- src/main/java/me/zbl/fullstack/FullstackApplication.java
+++ src/main/java/me/zbl/fullstack/FullstackApplication.java
@@ -254,4 +254,17 @@ public class FullstackApplication {
   public static void main(String[] args) {
     SpringApplication.run(FullstackApplication.class, args);
   }
+  /**
+   * 文件上传配置
+   * @return
+   */
+  @Bean
+  public MultipartConfigElement multipartConfigElement() {
+    MultipartConfigFactory factory = new MultipartConfigFactory();
+    //文件最大
+    factory.setMaxFileSize("102400KB"); //KB,MB
+    /// 设置总上传数据总大小
+    factory.setMaxRequestSize("1024000KB");
+    return factory.createMultipartConfig();
+  }
 }
